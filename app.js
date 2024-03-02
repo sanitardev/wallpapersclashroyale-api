72b905244ed05dc688f2dc9597f4b3c3b1c4950d
sanitardev <programmistsanitar@gmail.com>
2024-03-02 13:40:04 +0500

    cors фикс

--- app.js
+++ app.js
@@ -21,6 +21,12 @@ function generateUniqueFilename() {
     return `image_${timestamp}.jpg`;
 }
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 const sslOptions = {
   key: fs.readFileSync('.crt/private.key'),
   cert: fs.readFileSync('.crt/certificate.crt'),
